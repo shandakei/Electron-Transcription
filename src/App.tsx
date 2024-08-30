c9237de5d99f265aa3a8a1cbf409236c984185e9
Alexander <alexanderkkampfer@gmail.com>
2024-08-30 15:49:05 +1000

    new fork, team wide rebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import './App.css'
-import RecAudio from './components/RecordAudio/recordAudio'; 
-// import RemoteAudio from './utils/RemoteAudio/remoteAudio';
-
+// import { useState } from 'react';
+import './App.css';
+import RecAudio from './components/RecordAudio/recordAudio';
+// import SummarizeBtn from './components/summarizeBtn/summarizeBtn';
+// import { summarizeText } from './utils/summerizer/summerizer';
 
 function App() {
-  console.log('hello world');
+  // Initialize summary state
+  // const [summary, setSummary] = useState('');
+
+  // // Function to handle summarization
+  // async function handleSummarize() {
+  //   const text = "once a on a time there were three little pigs all lived in a house and i forgot the actual beginning of the story...";
+  //   const summary = await summarizeText(text);
+  //   setSummary(summary);
+  // }
 
   return (
     <>
-    
-    <div className="container">
-      <RecAudio />
-    </div>
-    <div>
-    </div>
-
+      <div className="container">
+        <RecAudio />
+        {/* <SummarizeBtn /> */}
+      </div>
+      <div>
+        {/* <button onClick={handleSummarize}>Test</button> */}
+        {/* <p>{summary}</p> */}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
